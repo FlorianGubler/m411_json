72e051c1d1607a7d31ac54d539b89b10c13f07f9
Florian Gubler <gubler.florian@gmx.net>
2022-01-28 19:39:07 +0100

    Add Local Safing Update

--- src/com/company/RandomUser.java
+++ src/com/company/RandomUser.java
@@ -3,20 +3,26 @@ package com.company;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class RandomUser {
+    public String ID;
     public String Gender;
     public String Firstname;
     public String Lastname;
 
-    public RandomUser(String gender, String firstname, String lastname){
-        this.Gender = gender; this.Firstname = firstname; this.Lastname = lastname;
+    public static String url = "https://randomuser.me/api/";
+
+    public RandomUser(String id, String gender, String firstname, String lastname){
+        this.ID = id; this.Gender = gender; this.Firstname = firstname; this.Lastname = lastname;
     }
 
+    //Default Constructor for JSON Mapper
     public RandomUser(){}
 
     public void printOut(){
@@ -24,11 +30,23 @@ public class RandomUser {
         System.out.println("Gender: " + this.Gender);
         System.out.println("Firstname: " + this.Firstname);
         System.out.println("Lastname: " + this.Lastname);
+        System.out.println("-> See Raw Data: " + url + "?format=pretty&seed=" + this.ID);
+    }
+
+    public void storeLocal(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nType in File Name to store Customer: ");
+        String Filepath = sc.nextLine();
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(Filepath), this);
+        } catch (Exception e){
+            System.out.println("Something went wrong by writing to File, please try again later");
+        }
+        System.out.println("User stored in local File '" + Filepath + "' successfully");
     }
 
     public static RandomUser getNewUser(){
-        String url = "https://randomuser.me/api/";
-
         System.out.println("Laoading Data...\n");
 
         HttpClient client = HttpClient.newHttpClient();
@@ -38,24 +56,37 @@ public class RandomUser {
                 .build();
 
         HttpResponse<String> response = null;
+
         try{
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Connection Failure, please try again later (Exit)");
-            System.exit(0);
+            System.out.println("Connection Failure, please try again later");
+            return null;
         }
 
         try{
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode userresults = objectMapper.readTree(response.body()).get("results").get(0);
+            JsonNode jsonparsed = objectMapper.readTree(response.body());
+            JsonNode userresults = jsonparsed.get("results").get(0);
 
-            return new RandomUser(userresults.get("gender").asText(), userresults.get("name").get("first").asText(), userresults.get("name").get("last").asText());
+            return new RandomUser(jsonparsed.get("info").get("seed").asText(), userresults.get("gender").asText(), userresults.get("name").get("first").asText(), userresults.get("name").get("last").asText());
         } catch (Exception e){
-            e.printStackTrace();
             System.out.println("Invalid Response (Exit)");
-            System.exit(0);
+            return null;
         }
-        return null;
+    }
+
+    public static RandomUser loadUserLocal(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nType in File Name to store Customer: ");
+        String Filepath = sc.nextLine();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            return objectMapper.readValue(new File(Filepath), RandomUser.class);
+        }catch (Exception e){
+            System.out.println("Userdata cant be loaded From File '" + Filepath + "'");
+            return null;
+        }
     }
 }
